254031c75f5e849759eaeed17cec55ee3287afa1
Dhanam Parekh <parekh.dhanam257@gmail.com>
2019-01-04 13:02:21 +0530

    Login and Signup

--- app/src/main/java/com/example/wayforlife/wayforlife/LoginActivity.java
+++ app/src/main/java/com/example/wayforlife/wayforlife/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.wayforlife.wayforlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,12 +9,17 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
     private ImageView wflLogo;
     private TextView Email, Password;
     private EditText editEmail, editPassword;
     private Button LoginButton;
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +27,20 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         InitializeFields();
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
+        if(currentUser!=null)
+        {
+            SendUserToHomeActivity();
+        }
+
+    }
+
+    private void SendUserToHomeActivity() {
+
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 
     private void InitializeFields() {
--- app/src/main/java/com/example/wayforlife/wayforlife/SignUpActivity.java
+++ app/src/main/java/com/example/wayforlife/wayforlife/SignUpActivity.java
@@ -2,12 +2,45 @@ package com.example.wayforlife.wayforlife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
+    private TextView FullName, Email, Password, ConfirmPassword, Mobile, State, City;
+    private EditText EditName, EditEmail, EditPassword, EditConfirmPassword, EditMobile;
+    private Button SignUpButton;
+    private Spinner EditState, EditCity;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        InitializeFields();
+    }
+
+    private void InitializeFields() {
+
+        FullName = (TextView)findViewById(R.id.fullname);
+        Email = (TextView)findViewById(R.id.email);
+        Password = (TextView)findViewById(R.id.password);
+        ConfirmPassword = (TextView)findViewById(R.id.confirm_password);
+        Mobile = (TextView)findViewById(R.id.mobile);
+        State = (TextView)findViewById(R.id.state);
+        City = (TextView)findViewById(R.id.city);
+        EditCity = (Spinner)findViewById(R.id.spinner_city);
+        EditState = (Spinner)findViewById(R.id.spinner_state);
+        SignUpButton = (Button)findViewById(R.id.sign_up);
+        EditName = (EditText)findViewById(R.id.edit_name);
+        EditPassword = (EditText)findViewById(R.id.edit_password);
+        EditConfirmPassword = (EditText)findViewById(R.id.edit_confirmpassword);
+        EditMobile = (EditText)findViewById(R.id.edit_mobile);
+        EditEmail = (EditText)findViewById(R.id.edit_email);
+
+
     }
 }
